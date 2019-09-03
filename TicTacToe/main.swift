8353c07f7b605750724cbcbac22baffed817d955
Liana Sargsyan <lnsrgsyn@gmail.com>
2019-09-03 16:07:25 +0400

    few changes

--- TicTacToe/main.swift
+++ TicTacToe/main.swift
@@ -6,49 +6,97 @@
 //  Copyright © 2019 17. All rights reserved.
 //
 
-enum Turn: String {
-    case x = "X"
-    case o = "O"
-    
-    var next: Turn {
-        switch self {
-        case .x: return .o
-        case .o: return .x
-        }
-    }
-}
 
-enum Mode: Int {
-    case easy = 1
-    case medium
-    case pro
-    
-    var size: Int{
-        switch self {
-        case .easy: return 3
-        case .medium: return 5
-        case .pro: return 7
-        }
-    }
-}
 
 class Game {
+    enum Turn: Character {
+        case x = "X"
+        case o = "O"
+        
+        var next: Turn {
+            switch self {
+            case .x: return .o
+            case .o: return .x
+            }
+        }
+    }
+    
+    enum Mode: Int {
+        case easy = 1
+        case medium
+        case pro
+        
+        var size: Int{
+            switch self {
+            case .easy: return 3
+            case .medium: return 5
+            case .pro: return 7
+            }
+        }
+    }
+    
+    enum GameError: Error {
+        case exitGame
+    }
+    
+    struct Board {
+        let size: Int
+        var grid = [[Character]]()
+        
+        init(mode: Int) {
+            self.size = mode
+            //grid= Array(repeating: Array(repeating: " ", count: mode.size), count: mode.size)
+        }
+        
+        subscript(move: Move) -> Character {
+            get {
+                //index check
+                return grid[move.i][move.j]
+            }
+            set {
+                //index check
+                grid[move.i][move.j] = newValue
+            }
+        }
+    }
+
     
     var playerTurn: Turn = .x
-    var move = (x: 0, y: 0)
-    var board = [[String]]()
+    var move = Move()
+    //var board: Board
+    var board = [[Character]]()
     var isActive = true
     let gameMode: Mode
     var moveCount = 0
     
-    
     init(mode: Mode) {
+        //board = Board(mode)
         gameMode = mode
+        //
         board = Array(repeating: Array(repeating: " ", count: mode.size), count: mode.size)
         printBoard(board)
     }
+    
+    struct Move {
+        let i: Int
+        let j: Int
+        
+        init() {
+            i = 0
+            j = 0
+        }
+        init?(elements: [Int]) {
+            guard elements.count == 2 else {
+                return nil
+            }
+            i = elements[0]
+            j = elements[1]
+        }
+    }
 
-    func printBoard(_ board: [[String]]) {
+    
+    
+    func printBoard(_ board: [[Character]]) {
         for _ in 0..<gameMode.size{
             print(" ---", terminator: "")
         }
@@ -66,107 +114,91 @@ class Game {
         }
     }
     
-    
-    func updateBoard(_ tuple: (Int, Int), _ player: Turn) {
-        board[tuple.0][tuple.1] = player.rawValue
+    func updateBoard(with move: Move, _ player: Turn) {
+        board[move.i][move.j] = player.rawValue
     }
     
-    func makeMove() {
-        print("Please make a move.\n\(playerTurn.rawValue): ", terminator: "")
-        
-        var m = readLine()!
-        var movArr = m.split(separator: " ")
-        var invalidMove = movArr.count != 2
-        
-        while invalidMove {
-            print("Please input a valid move.\n\(playerTurn.rawValue): ", terminator: "")
-            m = readLine()!
-            movArr = m.split(separator: " ")
-            invalidMove = movArr.count != 2
+    func canMake(_ move: Move) -> Bool {
+        guard move.i < gameMode.size, move.j < gameMode.size,
+            board[move.i][move.j].isWhitespace else {
+                return false
+        }
+        return true
+    }
+    
+    func makeMove() throws {
+        print("Please make a move (\(playerTurn.rawValue)): ", terminator: "")
+        guard let input = readLine() else {
+            throw GameError.exitGame
         }
         
-        invalidMove = Int(movArr[0])! >= gameMode.size || Int(movArr[1])! >= gameMode.size || board[Int(movArr[0])!][Int(movArr[1])!] != " "
-        
-        while invalidMove {
-            print("Please input a valid move.\n\(playerTurn.rawValue): ", terminator: "")
-            m = readLine()!
-            movArr = m.split(separator: " ")
-            invalidMove = Int(movArr[0])! >= gameMode.size || Int(movArr[1])! >= gameMode.size || board[Int(movArr[0])!][Int(movArr[1])!] != " " || movArr.count != 2
+        let elements = input.split(separator: " ").compactMap { Int($0) }
+        guard let move = Move(elements: elements), canMake(move) else {
+            print("Invalid move.")
+            try makeMove()
+            return
         }
         
-        
-        move.x = Int(movArr[0])!
-        move.y = Int(movArr[1])!
-        
-        updateBoard(move, playerTurn)
+        self.move = move
+        updateBoard(with: move, playerTurn)
         moveCount += 1
         printBoard(board)
-        
         
         checkState()
         playerTurn = playerTurn.next
     }
     
     func checkState() {
-        
-        //row
+        // row
         for j in 0..<board.count {
-            if board[move.x][j] != board[move.x][move.y] {
+            if board[move.i][j] != board[move.i][move.j] {
                 break
             }
-            if j == board.count - 1 {
-                print("**** \(playerTurn.rawValue) WON ****")
-                isActive = false
-            }
+            checkWinState(with: j)
         }
         
-        //column
+        // column
         for i in 0..<board.count {
-            if board[i][move.y] != board[move.x][move.y] {
+            if board[i][move.j] != board[move.i][move.j] {
                 break
             }
-            if i == board.count - 1 {
-                print("**** \(playerTurn.rawValue) WON ****")
-                isActive = false
-            }
+            checkWinState(with: i)
         }
         
-        //diagonal1
-        if move.x == move.y {
+        // diagonal1
+        if move.i == move.j {
             for k in 0..<board.count {
-                if board[k][k] != board[move.x][move.y] {
+                if board[k][k] != board[move.i][move.j] {
                     break
                 }
-                if k == board.count - 1 {
-                    print("**** \(playerTurn.rawValue) WON ****")
-                    isActive = false
-                }
+                checkWinState(with: k)
             }
         }
         
-        //diagonal2
-        if move.x + move.y == board.count - 1 {
+        // diagonal2
+        if move.i + move.j == board.count - 1 {
             for p in 0..<board.count {
-                if board[p][board.count - p - 1] != board[move.x][move.y] {
+                if board[p][board.count - p - 1] != board[move.i][move.j] {
                     break
                 }
-                if p == board.count - 1 {
-                    print("**** \(playerTurn.rawValue) WON ****")
-                    isActive = false
-                }
+                checkWinState(with: p)
             }
         }
         
-        //draw
+        // draw
         if moveCount == board.count * board.count {
             print("**** DRAW ****")
             isActive = false
         }
     }
     
+    func checkWinState(with count: Int) {
+        if count == board.count - 1 {
+            print("**** \(playerTurn.rawValue) WON ****")
+            isActive = false
+        }
+    }
 }
-
-
 
 var input: String?
 gameLoop: repeat {
@@ -176,31 +208,22 @@ gameLoop: repeat {
     guard
         let input = input,
         let inputNum = Int(input),
-        let mode = Mode(rawValue: inputNum) else {
+        let mode = Game.Mode(rawValue: inputNum) else {
             continue
     }
     
     let game = Game(mode: mode)
     
-    
     while game.isActive {
-        game.makeMove()
+        do {
+            try game.makeMove()
+        } catch Game.GameError.exitGame {
+            break gameLoop
+        } catch {
+            fatalError("Unknown error occurred!")
+        }
     }
    
 
 } while input != nil
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
